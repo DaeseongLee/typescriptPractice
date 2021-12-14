6538ddde745ab7ab0ec0a34c58ca32b6d0916455
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-12-14 22:33:48 +0900

    practice2

--- 2/game.ts
+++ 2/game.ts
@@ -1,12 +1,43 @@
 /**
  * Let's make a game 🕹
  */
-console.log(position); // { x: 0, y: 0}
-move('up');
-console.log(position); // { x: 0, y: 1}
-move('down');
-console.log(position); // { x: 0, y: 0}
-move('left');
-console.log(position); // { x: -1, y: 0}
-move('right');
-console.log(position); // { x: 0, y: 0}
+
+{
+    type Position = {
+        x: number,
+        y: number,
+    }
+
+    const position:Position = {
+        x: 0,
+        y: 0,
+    }
+    console.log(position); // { x: 0, y: 0}
+    move('up');
+    console.log(position); // { x: 0, y: 1}
+    move('down');
+    console.log(position); // { x: 0, y: 0}
+    move('left');
+    console.log(position); // { x: -1, y: 0}
+    move('right');
+    console.log(position); // { x: 0, y: 0}
+
+    type Move = 'up' | 'down' | 'left' | 'right';
+    function move(move: Move) {
+        switch (move) {
+            case 'up':
+                position.y++;
+                break;
+            case 'down':
+                position.y--;
+                break;
+            case 'left':
+                position.x--;
+                break;
+            case 'right':
+                position.x++;
+                break;
+            default: throw new Error('Unknown Error');
+        }
+    }
+}
