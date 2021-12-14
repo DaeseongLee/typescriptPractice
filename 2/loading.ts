f11ab9288e67f7f89c95f8e957c2738a688e47e4
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-12-14 22:40:08 +0900

    practice3

--- 2/loading.ts
+++ 2/loading.ts
@@ -20,7 +20,15 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(loadState: ResourceLoadState) {
+    switch (loadState.state) {
+      case 'loading': return console.log('👀 loading...');
+      case 'success': return console.log(loadState.response.body);
+      case 'fail': return console.log(loadState.reason);
+    }
+  }  
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
 }
