1e06595019345b0a3014867acb8c6a10ea284d7c
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2022-04-17 16:29:11 +0900

    type, interface는 언제 쓸까?

--- 9-advanced/9-1-interface.ts
+++ 9-advanced/9-1-interface.ts
@@ -55,3 +55,31 @@ type Person = {
     age: number,
 }
 type Name = Person['name'];
+
+type UnionType = 'left' | 'right';
+
+
+
+//그럼 언제 인터페이스를 쓸까??
+// 어떤 특정한 규격을 규정하는 거라면, type 보다는  interface를 사용하자
+//ex)
+interface CoffeeMaker {
+    coffeeBeans: number;
+    makeCoffee: (shots: number) => void;
+}
+
+class CoffeeMachine implements CoffeeMaker {
+    coffeeBeans: number;
+    makeCoffee(shots: number) {
+
+    }
+}
+
+// Type은 언제 쓸까??
+// 데이터를 담을 목적으로 사용할 때 타입을 쓴다
+type Position = {
+    x: number;
+    y: number;
+}
+
+const pos: Position = { x: 0, y: 0 };
