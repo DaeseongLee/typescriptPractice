2f8efdd7cb74fb2a9cbdae41288ee57d156d630b
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2022-04-17 17:11:37 +0900

    readonly type

--- 9-advanced/9-3-map.ts
+++ 9-advanced/9-3-map.ts
@@ -1,49 +1,51 @@
-type Video = {
-    title: string;
-    author: string;
-};
+{
+    type Video = {
+        title: string;
+        author: string;
+    };
 
-// type VideoOptional = {
-//     title?: string;
-//     author?: string;
-// }
+    // type VideoOptional = {
+    //     title?: string;
+    //     author?: string;
+    // }
 
-// type VideoReadOnly = {
-//     readonly title: string;
-//     readonly author: string;
-// }
+    // type VideoReadOnly = {
+    //     readonly title: string;
+    //     readonly author: string;
+    // }
 
-type Optional<T> = {
-    [P in keyof T]?: T[P]
-}
+    type Optional<T> = {
+        [P in keyof T]?: T[P]
+    }
 
-type VideoOptional = Optional<Video>;
-const videoOp: VideoOptional = {
-    title: 'a'
-}
+    type VideoOptional = Optional<Video>;
+    const videoOp: VideoOptional = {
+        title: 'a'
+    }
 
 
-type ReadOnly<T> = {
-    readonly [P in keyof T]: T[P];
-}
+    type ReadOnly<T> = {
+        readonly [P in keyof T]: T[P];
+    }
 
-const video: ReadOnly<Video> = {
-    title: 'aa',
-    author: '',
-}
+    const video: ReadOnly<Video> = {
+        title: 'aa',
+        author: '',
+    }
 
-type Nullable<T> = { [P in keyof T]: T[P] | null };
+    type Nullable<T> = { [P in keyof T]: T[P] | null };
 
-const obj3: Nullable<Video> = {
-    title: 'hi',
-    author: null
-}
+    const obj3: Nullable<Video> = {
+        title: 'hi',
+        author: null
+    }
 
-type Proxy<T> = {
-    get(): T;
-    set(value: T): void;
-}
+    type Proxy<T> = {
+        get(): T;
+        set(value: T): void;
+    }
 
-type Proxify<T> = {
-    [P in keyof T]: Proxy<T[P]>;
+    type Proxify<T> = {
+        [P in keyof T]: Proxy<T[P]>;
+    }
 }
