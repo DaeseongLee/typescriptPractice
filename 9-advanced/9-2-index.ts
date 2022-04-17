3a8b60c830efee06ad0dcc5bb89360d68c5ddb0c
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2022-04-17 16:36:39 +0900

    index type

---
+++ 9-advanced/9-2-index.ts
@@ -0,0 +1,33 @@
+{
+    const obj = {
+        name: 'lee'
+    }
+
+    obj.name;
+    obj['name']
+
+    type Animal = {
+        name: string;
+        age: number;
+        gender: 'male' | 'female';
+    }
+
+    type Name = Animal['name']; // string
+    const text: Name = 'hello';
+
+    type Gender = Animal['gender'];
+
+    type Keys = keyof Animal; // 'name' | 'age' | 'gender'
+    const key: Keys = 'gender';
+
+    type Person = {
+        name: string;
+        gender: Animal['gender'];
+    };
+    const person: Person = {
+        name: 'ellie',
+        gender: 'male',
+    }
+
+
+}
