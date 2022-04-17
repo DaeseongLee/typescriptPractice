c41e9ab8a55d15f794f2c092db684233e65a50c7
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2022-04-17 17:01:14 +0900

    Mapped Type

---
+++ 9-advanced/9-3-map.ts
@@ -0,0 +1,49 @@
+type Video = {
+    title: string;
+    author: string;
+};
+
+// type VideoOptional = {
+//     title?: string;
+//     author?: string;
+// }
+
+// type VideoReadOnly = {
+//     readonly title: string;
+//     readonly author: string;
+// }
+
+type Optional<T> = {
+    [P in keyof T]?: T[P]
+}
+
+type VideoOptional = Optional<Video>;
+const videoOp: VideoOptional = {
+    title: 'a'
+}
+
+
+type ReadOnly<T> = {
+    readonly [P in keyof T]: T[P];
+}
+
+const video: ReadOnly<Video> = {
+    title: 'aa',
+    author: '',
+}
+
+type Nullable<T> = { [P in keyof T]: T[P] | null };
+
+const obj3: Nullable<Video> = {
+    title: 'hi',
+    author: null
+}
+
+type Proxy<T> = {
+    get(): T;
+    set(value: T): void;
+}
+
+type Proxify<T> = {
+    [P in keyof T]: Proxy<T[P]>;
+}
